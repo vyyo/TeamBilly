1425f29e8538e150ca269c1e64165fd6ce2acf8d
vyyo <02lorenzo.vit@gmail.com>
2024-02-15 17:00:25 +0100

    Riordinati feedback

--- Billy/Assets/Billy/Scripts/VR/VRControls.cs
+++ Billy/Assets/Billy/Scripts/VR/VRControls.cs
@@ -37,12 +37,6 @@ KeyboardControls keyboardControls;
 
     void Update()
     {
-        //stanceInput();
-        //poseInput();
-        //Debug.Log(handStance);
-        //Debug.Log(handPose);
-
-        //This makes it so the player has to hold the pose for x seconds before the input is accepted. Changing pose resets the timer
         if(handStance && handPose)
         {
             heldTime -= Time.deltaTime;
@@ -64,29 +58,6 @@ KeyboardControls keyboardControls;
 
     public void vrStanceInput(int newStance)
     {
-        /*if(keyboardControls.Keyboard.StanceDef.IsPressed() && (currentStance == stances[0] || currentStance == stances[1]))
-        {
-            handStance = true;
-            currentStance = stances[1];
-            //Debug.Log(currentStance + " Stance!");
-        }
-        else if(keyboardControls.Keyboard.StanceNeut.IsPressed() && (currentStance == stances[0] || currentStance == stances[2]))
-        {
-            handStance = true;
-            currentStance = stances[2];
-            //Debug.Log(currentStance + " Stance!");
-        }
-        else if(keyboardControls.Keyboard.StanceAtk.IsPressed() && (currentStance == stances[0] || currentStance == stances[3]))
-        {
-            handStance = true;
-            currentStance = stances[3];
-            //Debug.Log(currentStance + " Stance!");
-        }
-        else
-        {
-            handStance = false;
-            currentStance = stances[0];
-        }*/
 
         if(newStance == 0)
         {
@@ -103,41 +74,6 @@ KeyboardControls keyboardControls;
 
     public void vrPoseInput(int newPose)
     {
-        /*if(keyboardControls.Keyboard.PoseSpock.IsPressed() && (currentPose == poses[0] || currentPose == poses[1]))
-        {
-            handPose = true;
-            currentPose = poses[1];
-            //Debug.Log(currentPose + "!");
-        }
-        else if(keyboardControls.Keyboard.PosePace.IsPressed() && (currentPose == poses[0] || currentPose == poses[2]))
-        {
-            handPose = true;
-            currentPose = poses[2];
-            //Debug.Log(currentPose + "!");
-        }
-        else if(keyboardControls.Keyboard.PoseMarcello.IsPressed() && (currentPose == poses[0] || currentPose == poses[3]))
-        {
-            handPose = true;
-            currentPose = poses[3];
-            //Debug.Log(currentPose + "!");
-        }
-        else if(keyboardControls.Keyboard.PosePistola.IsPressed() && (currentPose == poses[0] || currentPose == poses[4]))
-        {
-            handPose = true;
-            currentPose = poses[4];
-            //Debug.Log(currentPose + "!");
-        }
-        else if(keyboardControls.Keyboard.PoseOk.IsPressed() && (currentPose == poses[0] || currentPose == poses[5]))
-        {
-            handPose = true;
-            currentPose = poses[5];
-            //Debug.Log(currentPose + "!");
-        }
-        else
-        {
-            handPose = false;
-            currentPose = poses[0];
-        }*/
 
         if(newPose == 0)
         {
@@ -156,14 +92,14 @@ KeyboardControls keyboardControls;
     {
         currentPose = poses[0];
         handPose = false;
-        Debug.Log("POSE RESET: " + handPose);
+        //Debug.Log("POSE RESET: " + handPose);
     }
 
     public void vrStanceReset()
     {
         currentStance = stances[0];
         handStance = false;
-        Debug.Log("STANCE RESET: " + handStance);
+        //Debug.Log("STANCE RESET: " + handStance);
     }
 
     void Attack()
